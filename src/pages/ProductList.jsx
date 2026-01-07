c458d5cd54442c8af2768d2b3c0c2f45bb275792
sagnikbera <sagnikberaofficial@gmail.com>
2026-01-07 12:13:29 +0530

    set up search route

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ProductTable from '../components/ProductTable';
+import { FaSearch } from 'react-icons/fa';
 
 const ProductList = () => {
   const navigate = useNavigate();
   return (
     <div className="p-6">
-      <div className="flex justify-end mb-4">
+      <div className="flex justify-end mb-4 gap-6">
+        <button
+          className="bg-black text-white px-3 py-1 rounded font-semibold"
+          onClick={() => navigate('/search')}
+        >
+          <span className="flex justify-center items-center gap-3">
+            <FaSearch /> Search
+          </span>
+        </button>
+
         <button
           className="bg-blue-500 px-3 py-1 rounded font-semibold"
           onClick={() => navigate('/add')}
